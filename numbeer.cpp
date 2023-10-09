5f50fd01680f2d66bea88b015b32cc550b2d1249
Cheprichate <petrovich15071998@mail.ru>
2023-10-09 19:10:26 +0400

    Добавьте файлы проекта.

---
+++ numbeer.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+int num;
+int counter;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите число\n";
+    std::cin >> num;
+    if (num == 0) {
+        counter++;
+    }
+    while (num > 0) {
+        counter++;
+        num /= 10;
+    }
+    std::cout << "Цифр  числе " << counter;
+}
